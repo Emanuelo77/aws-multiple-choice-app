c0d67226d2fbebf5f84b6bb9613fc5bb0455d7c0
Gülcan Kamat <gulcankamat@gmail.com>
2024-06-03 12:43:21 +0200

    Adding the first steps our App Project.

---
+++ aws_benutzerkonto.js
@@ -0,0 +1,7 @@
+const AWS = require('aws-sdk');
+
+AWS.config.update({
+  region: 'YOUR_AWS_REGION',
+  accessKeyId: 'YOUR_ACCESS_KEY_ID',
+  secretAccessKey: 'YOUR_SECRET_ACCESS_KEY'
+});
---
+++ src/ExampleComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ExampleComponent = () => {
+  return (
+    <div>
+      <h1>Willkommen zur AWS-Multiple-Choice-App!</h1>
+      <p>Dies ist eine Beispielkomponente.</p>
+    </div>
+  );
+};
+
+export default ExampleComponent;
